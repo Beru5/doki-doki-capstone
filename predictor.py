c544e5722e3cda8439883f0c8ae0154218bf7054
Bernardo Ng <162414313+Beru5@users.noreply.github.com>
2026-06-12 12:00:21 +0700

    minor fix

--- predictor.py
+++ predictor.py
@@ -11,16 +11,13 @@ MODEL_PATH = PROJECT_ROOT / "model_dir" / "medbuddy_model.joblib"
 model = load(MODEL_PATH)
 
 def predict(input_data: dict):
-    # Urutan fitur harus konsisten dengan urutan saat training
     feature_order = [
         "age", "sex", "cp", "trestbps", "chol", "fbs", 
         "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"
     ]
     
-    # Ambil values menjadi format list nested [[val1, val2, ...]]
     features = [[input_data[key] for key in feature_order]]
 
-    # Prediksi langsung menggunakan list (scikit-learn support ini)
     prediction = int(model.predict(features)[0])
     probability = float(model.predict_proba(features)[0][1])
 
